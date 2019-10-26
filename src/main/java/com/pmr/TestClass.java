db27330091b1539521544c63f1a6c4d294217348
Paaredi, Muthyam Reddy <pmr1244@gmail.com>
2019-10-26 13:36:16 +0530

    initial commit

---
+++ src/main/java/com/pmr/TestClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.pmr;
+
+/**
+ * @author pmr
+ *
+ */
+public class TestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
